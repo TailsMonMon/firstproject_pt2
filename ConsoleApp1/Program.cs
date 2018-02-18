ffefcbf7b465a0985ebbf64bd4173acb1ab69f53
Monica Björk <m.bjork95@gmail.com>
2018-02-18 18:58:08 +0100

    Working on documentation. Continue on line 142.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,34 +12,72 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            var intVal = 1234;
-            Console.WriteLine("intVal Type : {0}", intVal.GetType());   //To know what datatype it is.
-            Console.WriteLine("");
+            //      Program assumes what datatype it is
+            var intVal = 1234;  // 32bit
+            Console.WriteLine("{0} is a : {1}", 
+                intVal, intVal.GetType());      // To know what datatype it is.
+
+            var intVal2 = 3.14159265359; // Double
+            Console.WriteLine("{0} is a : {1}", 
+                intVal2, intVal2.GetType());
+
+            var intVal3 = 12345654321; // 64bit
+            Console.WriteLine("{0} is a : {1}", 
+                intVal3, intVal3.GetType());
+
+            var intVal4 = "Hellu"; // String
+            Console.WriteLine("{0} is a : {1}", 
+                intVal4, intVal4.GetType());
+
+            var intVal5 = true; // Boolean
+            Console.WriteLine("{0} is a : {1}",
+                intVal5, intVal5.GetType());
+            
+            Console.WriteLine(""); 
 
 
-                // Arrays (starts at 0)
+            //      Arrays 
             int[] favNums = new int[3];
-            favNums[0] = 23;
+            favNums[0] = 12;    // 0 = 12
             Console.WriteLine("favNum 0 : {0}", favNums[0]);
             Console.WriteLine("");
 
-            object[] randomArray = { "Anna", 23, 3.14 };    // Several datatypes in one array. String, int, double/decimal.
-            Console.WriteLine("randomArray Anna : {0}", randomArray[0].GetType());
-            Console.WriteLine("randomArray 23 : {0}", randomArray[1].GetType());
-            Console.WriteLine("randomArray 3.14 : {0}", randomArray[2].GetType());
-            Console.WriteLine("Array Size : {0}", randomArray.Length);
 
-            for (int i = 0; i < randomArray.Length; i++)
+            object[] randomArray = { "Anna", 23, 3.14 };    // Several datatypes in one array. String, int, double/decimal.
+
+            Console.WriteLine("randomArray Anna : {0}", randomArray[0].GetType());  // First datatype
+            Console.WriteLine("randomArray 23 : {0}",   randomArray[1].GetType());  // Second datatype
+            Console.WriteLine("randomArray 3.14 : {0}", randomArray[2].GetType());  // Third datatype
+            Console.WriteLine("Array Size : {0}",       randomArray.Length);
+
+            for (int i = 0; i < randomArray.Length; i++)    // i++ is a loop
             {
                 Console.WriteLine("Array {0} : Value : {1}", i, randomArray[i]);
             }
             Console.WriteLine();
 
-            string[] customers = { "Pelle", "Nemi", "Stig", "Berra" };
-            var employees = new[] { "Ina", "Esbjörn", "My", "Eleonore" };
 
 
-                // 2D Arrays
+            //      1D Array     //  0        1       2       3       4     5
+            string[] array1D = { "Pelle", "Nemi", "Stig", "Berra", "My", "Ina" };
+            for (int i = 0; i < array1D.GetLength(0); i++)  // i is equal to 0; When array1D is bigger than i; do i++.
+            {                                               // i is what object the console will start write. 
+                Console.Write(" {0} ", array1D[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            var another1DArray = new[] { "Pelle", "Nemi", "Stig", "Berra", "My", "Ina" };
+            for (int i = 2; i < another1DArray.GetLength(0); i++)   // "Hides" int 0 and 1 since i = 2.
+            {
+                Console.Write(" {0} ", another1DArray[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            //      2D Array
             String[,] array2D = new string[3, 4]
             {
                 { " AA ", " AB ", " AC ", " AD " },
@@ -47,49 +85,71 @@ namespace ConsoleApp1
                 { " CA ", " CB ", " CC ", " CD " }
             };
 
-            for (int i = 0; i < array2D.GetLength(0); i++)         // 
+            for (int i = 0; i < array2D.GetLength(0); i++)  // 
             {
-                for (int j = 0; j < array2D.GetLength(0); j++)    //
+                for (int j = 0; j < array2D.GetLength(0); j++)  //
                 {
                     Console.Write(" {0} ", array2D[i, j]);         //
-                }// Console.WriteLine... Will make ''break'' the table.
+                }// Console.WriteLine... Will "break" the table.
                 Console.WriteLine();
             }
-            Console.WriteLine("a value: {0}", array2D.GetValue(1, 1));   // AA = 00, AB = 01, AC = 02. BA = 10, BB = 11, BC = 12, etc. 
+            Console.WriteLine("a value: {0}", array2D.GetValue(1, 1));   // AA = 00,  BB = 11, CC = 22, etc.
             Console.WriteLine();
             Console.WriteLine();
             // It is possible to make 3D arrays and even 4D arrays.
 
 
-                // For each
-            int[] randomNumbers = { 3, 5, 2, 1, 4 };    ///(0, 1, 2, 3, 4)
-            PrintArray(randomNumbers, "ForEach");
-            Console.WriteLine("1 at Index : {0}", Array.IndexOf(randomNumbers, 1));
+            /*//    3D Array
+            String[,,] array3D = new string[3, 4, 2]
+            {
+                { { " AA ", "1" }, { " AB ", "2" },  { " AC ", "3" },  { " AD ", "4" } },
+                { { " BA ", "5" }, { " BB ", "6" },  { " BC ", "7" },  { " BD ", "8" } },
+                { { " CA ", "9" }, { " CB ", "10" }, { " CC ", "11" }, { " CD ", "12" } }
+            };
 
-            randomNumbers.SetValue(0, 1);   // 1 -> 0 aka 5 -> 0 (See line 64)
+            for (int i = 0; i < array3D.GetLength(0); i++)  // 
+            {
+                for (int j = 0; j < array3D.GetLength(0); j++)  //
+                {
+                    for (int k = 0; k < array3D.GetLength(0); k++)  //
+                    {
+                        Console.Write(" {0} ", array3D[i, j, k]);         // Error: System.IndexOutOfRangeException
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
             Console.WriteLine();
+            Console.WriteLine();
+            */
+
+
+            //      For each
+            int[] randomNumbers = { 3, 5, 2, 1, 4 };    // (0, 1, 2, 3, 4)
+            PrintArray(randomNumbers, "ForEach");
+            Console.WriteLine("1 is at Index : {0}", Array.IndexOf(randomNumbers, 1));
+
+            randomNumbers.SetValue(0, 1);   // Value 1 -> 0. So 5 -> 0
 
             Array.Reverse(randomNumbers);
-            PrintArray(randomNumbers, "Reverse");
-            Console.WriteLine();
+            PrintArray(randomNumbers, "Reverse");   // Reverse the order of the integers
 
             Array.Sort(randomNumbers);
-            PrintArray(randomNumbers, "Sort");
-            Console.WriteLine();
+            PrintArray(randomNumbers, "Sort");  // Sort the order of the integers
             Console.WriteLine();
 
 
-                // Copy arrays
-            int[] sourceArray = { 1, 2, 3 };
-            int[] destinationArray = new int[2];
-            int startIndex = 0;
-            int length = 2;
-            Array.Copy(sourceArray, startIndex, destinationArray, startIndex, length);
-            PrintArray(destinationArray, "Copy");
-            Console.WriteLine();
+            //      Copy arrays
+            int[] originalArray = { 01, 02, 03, 04, 05 };  // The original array. Obviosly.
+            int[] copiedArray = new int[2];     //
+            int   startIndex = 0;               // At what index it will start to copy.
+            int   length = 2;                   // 
+            Array.Copy(originalArray, startIndex, copiedArray, startIndex, length); // It _has_ to be in that order.
+            PrintArray(originalArray, "Original");
+            PrintArray(copiedArray, "Copy");
 
             Array anotherArray = Array.CreateInstance(typeof(int), 10); // Creates and interger-array with a total of 10 spaces
-            sourceArray.CopyTo(anotherArray, 2);    // Number of spaces before the copies
+            originalArray.CopyTo(anotherArray, 3);    // Number of spaces, aka 0 before the actual copy.
 
             foreach (int m in anotherArray)
             {
